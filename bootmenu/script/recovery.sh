3039331566ce029bee0853b27e065db02f47f35c
Endless7 <2288136aaa@gmail.com>
2011-11-12 12:58:54 +0800

    Load overclock setting in latest recovery and solve sd card mounting issue.

--- bootmenu/script/recovery.sh
+++ bootmenu/script/recovery.sh
@@ -8,42 +8,34 @@ export PATH=/sbin:/system/xbin:/system/bin
 
 ######## Main Script
 
-## /tmp folder can be a link to /data/tmp, bad thing !
+PART_DATA=/dev/block/mmcblk1p26
+PART_SYSTEM=/dev/block/mmcblk1p21
+
+# Moto 2.3.3 /tmp is a link to /data/tmp, bad thing ! &&
 [ -L /tmp ] && rm /tmp
-mkdir -p /tmp
+[ -L /etc ] && rm /etc
 
-mkdir -p /pds
-mkdir -p /res
-
-rm -f /etc
+mkdir /tmp
 mkdir /etc
+mkdir /res
 
 # hijack mke2fs & tune2fs CWM3
 rm -f /sbin/mke2fs
 rm -f /sbin/tune2fs
 rm -f /sbin/e2fsck
 
-rm -f /sdcard
-mkdir /sdcard
-
-chmod 755 /sbin
-chmod 755 /res
-
 cp -r -f /system/bootmenu/recovery/res/* /res/
-cp -p -f /system/bootmenu/recovery/sbin/* /sbin/
+cp -r -f /system/bootmenu/recovery/sbin/* /sbin/
+chmod 755 /sbin/*
 cp -p -f /system/bootmenu/script/recoveryexit.sh /sbin/
+cp /system/bootmenu/recovery/recovery.fstab /etc/recovery.fstab
 
 if [ ! -f /sbin/recovery ]; then
-  ln -s /sbin/recovery_stable /sbin/recovery
+    ln -s /sbin/recovery_stable /sbin/recovery
 fi
 
-chmod +rx /sbin/*
-
-rm -f /sbin/postrecoveryboot.sh
-
-if [ ! -e /etc/recovery.fstab ]; then
-  cp /system/bootmenu/recovery/recovery.fstab /etc/recovery.fstab
-fi
+# for ext3 format
+cp /system/etc/mke2fs.conf /etc/
 
 mkdir -p /cache/recovery
 touch /cache/recovery/command
@@ -51,65 +43,48 @@ touch /cache/recovery/log
 touch /cache/recovery/last_log
 touch /tmp/recovery.log
 
-killall adbd
-sleep 1
+# load overclock settings to reduce heat and battery use
+/system/bootmenu/script/overclock.sh
+
+#CWM below5.0.3.0 don't need this.
+# mount image of pds, for backup purpose (4MB)
+#[ ! -d /data/data ] && mount -t ext3 -o rw,noatime,nodiratime,errors=continue $PART_DATA /data
+#if [ ! -f /data/pds.img ]; then
+#    /system/etc/init.d/02pdsbackup
+#    umount /pds
+#    losetup -d /dev/block/loop7
+#fi
+#cp /data/pds.img /tmp/pds.img
+#if [ -f /tmp/pds.img ] ; then
+#    mkdir -p /pds
+#    umount /pds 2>/dev/null
+#    losetup -d /dev/block/loop7 2>/dev/null
+#    losetup /dev/block/loop7 /tmp/pds.img
+#    busybox mount -o rw,nosuid,nodev,noatime,nodiratime,barrier=1 /dev/block/loop7 /pds
+#fi
+
+## adbd start
+
+rm -f /sbin/adbd
 ps | grep -v grep | grep adbd
 ret=$?
 
 if [ ! $ret -eq 0 ]; then
-   # chmod 755 /system/bootmenu/script/adbd.sh
-   # /system/bootmenu/script/adbd.sh
-
-   # don't use adbd here, will load many android process which locks /system
-   killall adbd
-   killall adbd.root
+  chmod 755 /system/bootmenu/script/adbd.sh
+  /system/bootmenu/script/adbd.sh
 fi
 
+
 #############################
-# mount in /sbin/postrecoveryboot.sh
-umount /system
-
-usleep 50000
-mount -t ext3 -o rw,noatime,nodiratime /dev/block/mmcblk1p21 /system
-
-# retry without type & options if not mounted
-[ ! -f /system/build.prop ] && mount -o rw /dev/block/mmcblk1p21 /system
-
-# set red led if problem with system, green led else
-
-echo 0 > /sys/class/leds/red/brightness
-echo 0 > /sys/class/leds/green/brightness
-echo 0 > /sys/class/leds/blue/brightness
-[ ! -f /system/build.prop ] && echo 1 > /sys/class/leds/red/brightness
-# [ -f /system/build.prop ] && echo 1 > /sys/class/leds/green/brightness
-
+## mount in /sbin/postrecoveryboot.sh
+umount -l /system
+umount -l /data
+umount -l /cache
 #############################
 
 # turn on button backlight (back button is used in CWM Recovery 3.x)
 echo 1 > /sys/class/leds/button-backlight/brightness
 
-
 /sbin/recovery
-
-
-# Post Recovery (back to bootmenu)
-
-# bootmenu doesnt support buttons
-echo 0 > /sys/class/leds/button-backlight/brightness
-
-# remount system & data if unmounted
-[ ! -d /data/data ] &&         mount -t ext3 -o rw,noatime,nodiratime,errors=continue /dev/block/mmcblk1p26 /data
-[ ! -f /system/build.prop ] && mount -t ext3 -o rw,noatime,nodiratime,errors=continue /dev/block/mmcblk1p21 /system
-
-if [ -f /system/build.prop ] ; then
-	echo 0 > /sys/class/leds/red/brightness
-	echo 0 > /sys/class/leds/green/brightness
-	echo 1 > /sys/class/leds/blue/brightness
-else
-	echo 1 > /sys/class/leds/red/brightness
-	echo 0 > /sys/class/leds/green/brightness
-	echo 0 > /sys/class/leds/blue/brightness
-fi
-
 
 exit
