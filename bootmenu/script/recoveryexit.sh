de8df6668383d6a92c89eda9404f78c1df5648fb
Endless7 <2288136aaa@gmail.com>
2011-11-21 21:08:02 +0800

    it conflict with sd fix. bootmenu still has sd mounting issue,which can not use mass storage mod in most of the time.

--- bootmenu/script/recoveryexit.sh
+++ bootmenu/script/recoveryexit.sh
@@ -9,16 +9,16 @@ echo 1 > /sys/class/leds/button-backlight/brightness
 usleep 50000
 echo 0 > /sys/class/leds/button-backlight/brightness
 
-mount -t ext3 -o nosuid,nodev,noatime,nodiratime,barrier=1 /dev/block/mmcblk1p24 /cache
-mount -t ext3 -o nosuid,nodev,noatime,nodiratime,barrier=1 /dev/block/mmcblk1p21 /system
+#mount -t ext3 -o nosuid,nodev,noatime,nodiratime,barrier=1 /dev/block/mmcblk1p24 /cache
+#mount -t ext3 -o nosuid,nodev,noatime,nodiratime,barrier=1 /dev/block/mmcblk1p21 /system
 
-rm -f /cache/recovery/bootmode.conf
-touch /cache/recovery/bootmode.conf
-echo "bootmenu" > /cache/recovery/bootmode.conf;
-ln -s /system/bin/bootmenu /sbin/bootmenu
+#rm -f /cache/recovery/bootmode.conf
+#touch /cache/recovery/bootmode.conf
+#echo "bootmenu" > /cache/recovery/bootmode.conf;
+#ln -s /system/bin/bootmenu /sbin/bootmenu
 
-sync
+#sync
 
-/sbin/bootmenu
+#/sbin/bootmenu
 
-exit
+#exit
