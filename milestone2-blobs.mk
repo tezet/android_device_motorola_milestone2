e86294cea0f0f40ca22f852e8ff4480b5e2a7ca4
Tomasz Zajac <tomasz.zajac@post.pl>
2012-10-19 20:40:41 +0200

    Remove zram script
    Change-Id: Ice919e0a4252ea91d106c23e5fc19e13919acc47

--- milestone2-blobs.mk
+++ milestone2-blobs.mk
@@ -44,7 +44,6 @@ PRODUCT_COPY_FILES += \
 	$(device_path)/etc/init.d/02baseband:system/etc/init.d/02baseband \
 	$(device_path)/etc/init.d/03firstboot:system/etc/init.d/03firstboot \
 	$(device_path)/etc/init.d/05mountsd:system/etc/init.d/05mountsd \
-	$(device_path)/etc/init.d/10zram:system/etc/init.d/10zram \
 	$(device_path)/etc/init.d/90multitouch:system/etc/init.d/90multitouch \
 	$(device_path)/etc/profile:system/etc/profile \
 	$(device_path)/etc/sysctl.conf:system/etc/sysctl.conf \
